43efb63c0063b534979aa90199d904bb0a54a0a1
Rifki Indra Tamaya <105766757+rifkiindra@users.noreply.github.com>
2022-10-23 22:11:32 +0700

    Add files via upload

---
+++ codingan visual studio code gym cak to rek.js
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company Profile</title>
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous"/>
+   <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">GYM CAK TO REK</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          </button>
+          <div class="collapse navbar-collapse text-right" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#Kelas">Kelas</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Training">Training</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Fasilitas">Fasilitas</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#Member Benefit">Member Benefit</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#Staff">Staff</a>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#Kontak Kami">Kontak Kami</a>
+            </form>
+          </div>
+        </div>
+    </nav>
+    <div class="container-fluid banner">
+      <div class="container text-center">
+        <h4 class="display-6">SELAMAT DATANG DIWEBSITE KAMI</h4>
+        <h3 class="display-1">SALAM OLAHRAGA!</h3>
+      </div>
+    </div>
+    <div class="container-fluid pt-5 pb-5 bg-light">
+      <div class="container text text-center">
+        <h2 class="display-3" id="Kelas">Kelas</h2>
+        <p>Pada Halaman ini akan ditampilkan menu kelas yang sesuai dengan batasan usia.</p>
+    
+      <div class="row pt-4 gx-4 gy-4">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/istockphoto-978997294-612x612-1.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="GYM KIDS">GYM KIDS</h5>
+              <p class="card-text">GYMNASTIC KIDS dikhususkan untuk anak-anak yang ber usia mulai 2 tahun ke atas.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/muscle-press-pose-athlete-workout-hd-wallpaper-preview.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="GYM DEWASA">GYM DEWASA</h5>
+              <p class="card-text">GYM DEWASA dikhusukan untuk orang berusia 18 tahun keatas.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/8f1b2aa3403074a8bd7b003b2e00cfbc.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="GYM LANSIA">GYM LANSIA</h5>
+              <p class="card-text">GYM LANSIA di khususkan untuk orang yang berusia 65 tahun keatas.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+      </div>
+    </div>
+    <div class="container-fluid pt-5 pb-5 bg-light">
+      <div class="container text text-center">
+        <h2 class="display-3" id="Training">Training</h2>
+        <p>Pada Halaman ini akan ditampilkan dengan macam-macam olahraga yang ada di GYM Cak To Rek</p>
+      <div class="row pt-4 gx-4 gy-4">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/yoga.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="YOGA">YOGA</h5>
+              <p class="card-text">Yoga adalah olahraga tubuh dan pikiran yang fokus pada kekuatan, kelenturan, serta pernapasan demi peningkatan kesehatan mental dan fisik.
+              </p>
+              <a href="#" class="btn btn-primary">Klik untuk mengatur jadwal latihan</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/zumba77.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="SENAM ZUMBA">SENAM ZUMBA</h5>
+              <p class="card-text">Senam zumba adalah latihan fisik yang memadukan kombinasi tari salsa dan gerakan aerobik yang menyenangkan.</p>
+              <a href="#" class="btn btn-primary">Klik untuk mengatur jadwal latihan</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/body-pum77.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="BODY PUMP">BODY PUMP</h5>
+              <p class="card-text">Body Pump adalah olahraga berkelompok berdurasi 30-60 menit per sesi dengan tujuan membentuk otot bagi pemula dan pengencangan otot bagi mereka yang sudah terbiasa olahraga.</p>
+              <a href="#" class="btn btn-primary">Klik untuk mengatur jadwal latihan</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/bodycombat_fb-sharing.png"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="BODY COMBAT">BODY COMBAT</h5>
+              <p class="card-text">BodyCombat adalah salah satu dari kelas kebugaran yang dibuat dan didistribusikan oleh Les Mills International yang menggabungkan seni bela diri dari seluruh dunia.</p>
+              <a href="#" class="btn btn-primary">Klik untuk mengatur jadwal latihan</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/abs.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="ABS">ABS</h5>
+              <p class="card-text">Abs workout adalah satu istilah yang digunakan untuk menyebut latihan bagian perut, sehingga bisa menjadi kuat dan memiliki bentuk yang ideal. </p>
+              <a href="#" class="btn btn-primary">Klik untuk mengatur jadwal latihan</a>
+            </div>
+          </div>
+        </div>
+    <div class="container-fluid pt-5 pb-5 bg-light">
+      <div class="container text text-center">
+        <h2 class="display-3" id="Fasilitas">Fasilitas</h2>
+        <p>Pada Halaman ini akan menampilkan fasilitas utama yang tersedia di GYM CAK TO REK.</p>
+      <div class="row pt-4 gx-4 gy-4">
+        <div class="col-md-4">
+          <div class="card crop-img">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/Perth-Hills-and-Chills-03.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+            <div class="card-body">
+              <h5 class="YOGA AREA">YOGA AREA</h5> 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card crop-img">
+            <img src="http://localhost/wordpress/wp-content/uploads/2022/10/zumba.jpg"
+             class="card-img-top" 
+             width="200"
+             height="200"
+             />
+           <div class="card-body">
+             <h5 class="ZUMBA AREA">ZUMBA AREA</h5> 
+          </div>
+         </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card crop-img">
+         <img src="http://localhost/wordpress/wp-content/uploads/2022/10/body-pum-body.jpg"
+         class="card-img-top" 
+         width="200"
+        height="200"
+      />
+      <div class="card-body">
+        <h5 class="Body Pump, Body Combat & ABS AREA">BodyPump, Combat&ABS AREA</h5> 
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+  <div class="card crop-img">
+   <img src="http://localhost/wordpress/wp-content/uploads/2022/10/a3014aaef9.jpg"
+   class="card-img-top" 
+   width="200"
+  height="200"
+/>
+<div class="card-body">
+  <h5 class="Kamar Mandi">Kamar Mandi</h5> 
+</div>
+</div>
+</div>
+<div class="col-md-4">
+  <div class="card crop-img">
+   <img src="http://localhost/wordpress/wp-content/uploads/2022/10/massage-towel-service.jpg"
+   class="card-img-top" 
+   width="200"
+  height="200"
+/>
+<div class="card-body">
+  <h5 class="Towel Servis & SPA">Towel Servis & SPA</h5> 
+</div>
+</div>
+</div>
+<div class="col-md-4">
+  <div class="card crop-img">
+   <img src="http://localhost/wordpress/wp-content/uploads/2022/10/lovepik-underground-parking-lot-picture_501278272.jpg"
+   class="card-img-top" 
+   width="200"
+  height="200"
+/>
+<div class="card-body">
+  <h5 class="Parkiran luas">Parkiran luas</h5> 
+</div>
+</div>
+</div>
+<div class="container-fluid pt-5 pb-5">
+  <div class="container">
+      <h2 class="display-3 text-center" id="Member Benefit">Member Benefit</h2>
+      <p class="text-center">
+          Manfaat dan keuntungan dari member benefit
+      </p>
+      <div class="clearfix pt-5"><img src="http://localhost/wordpress/wp-content/uploads/2022/10/Gym-Free-Weights__FitWzIwMDAsMjAwMF0.jpg" 
+          class="col-md-6 float-md-end mb-3 crop-img" 
+          width="300" 
+          height="300"
+          />
+          <p>Karena memang tempat tersebut menjadi pusat kebugaran, alat-alat yang disediakan pun relatif lengkap. 
+            Kamu bisa menemukan alat-alat mulai dari yang kecil sampai mesin-mesin yang besar. Pastinya, pilihan tersebut pun bisa
+            dimanfaatkan selama menjadi member karena kamu bisa datang kapan saja.
+            Kamu pun bisa menyewa personal trainer untuk membantu latihan kamu di sana. Kamu bisa mengeset goal kamu dan pastinya orang-orang terlatih tersebut akan membantu memilihkan latihan terbaik untuk kamu lakukan. Tugas personal trainer pun bukan hanya sampai situ. Kamu pun akan diberikan motivasi dan saran asupan makanan untuk memaksimalkan latihan.
+            Kalau pun kamu bosan latihan sendiri, kamu bisa mengikuti kelas-kelas yang sudah terjadwal rutin. Kamu bisa mengikuti kelas sesuai dengan keinginan kamu. Pastinya semua akan bermanfaat untuk tubuh kamu. Kamu pun bisa menambah teman dan koneksi saat mengikuti kelas-kelas gym tersebut.
+          </p>
+      </div>
+  </div>
+</div>
+  <div class="container text-center">
+    <h2 class="display-3" id="Staff">Tim Kami</h2>
+    <p>Berikut jajaran staff GYM Cak To Rek</p>
+    <div class="row pt-4 gx-4 gy-4">
+      <div class="col-md-4 text-center tim">
+        <img src="http://localhost/wordpress/wp-content/uploads/2022/10/0200882025.jpg" 
+        class="rounded-circle mb-3" 
+        />
+        <h4>Rifki</h4>
+        <p>Trainer</p>
+        <p>
+          <a href=""></i></a>
+        </p>
+      </div>
+      <div class="col-md-4 text-center tim">
+        <img src="http://localhost/wordpress/wp-content/uploads/2022/10/0200882025.jpg" 
+        class="rounded-circle mb-3" 
+        />
+        <h4>indra</h4>
+        <p>Personal trainer</p>
+        <p>
+          <a href=""></i></a>
+        </p>
+      </div>
+      <div class="col-md-4 text-center tim">
+        <img src="http://localhost/wordpress/wp-content/uploads/2022/10/0200882025.jpg" 
+        class="rounded-circle mb-3" 
+        />
+        <h4>Tamaya</h4>
+        <p>Call Center</p>
+        <p>
+          <a href=""></i></a>
+        </p>
+      </div>
+      <div class="container-fluid pt-5 pb-5 Kontak">
+        <div class="container">
+          <h2 class="display-3 text-center" id="Kontak Kami">Kontak Kami</h2>
+          <p class="text-center">Silahkan tinggalkan pertanyaan jika ada yang kurang jelas
+          </p>
+          <div class="row pb-3"></div>
+            <div class="col-md-6">
+              <input class="form-control form-control-lg mb-3"
+              type="text"
+              placeholder="Nama"
+              />
+            </div>
+          <div class="row pb-3">
+           <div class="col-md-6">
+              <input class="form-control form-control-lg mb-3"
+              type="text"
+              placeholder="Email"
+              />
+            </div>
+          <div class="row pb-3">
+           <div class="col-md-6">
+              <input class="form-control form-control-lg mb-3"
+              type="text"
+              placeholder="No. Phone"
+              />
+            </div>
+            <div class="col-md-6"></div>
+             <textarea class="form-control form-control-lg" rows="5"></textarea>
+            </div>
+          </div>
+          <div class="col-md-3 mx-auto text-center">
+            <button type="button" class="btn btn-danger btn-lg">
+              Kirim Pesan
+            </button>
+          </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" 
+    integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk"
+     crossorigin="anonymous"
+     ></script>
+</body>
+</html>
